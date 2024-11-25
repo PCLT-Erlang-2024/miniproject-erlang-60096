b71830ec67bab5a161035228afd447f54bd10fbd
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2024-11-25 08:05:34 +0000

    Task 2 done - package size is now random within interval [MinPackageSize, MaxPackageSize]

--- task2/factory.erl
+++ task2/factory.erl
@@ -1,16 +1,18 @@
 -module(factory).
--export([start/5]).
+-export([start/6]).
 
-start(NumPackages, NumBelts, NumTrucks, PackageSize, TruckCapacity) ->
+start(NumPackages, NumBelts, NumTrucks, MinPackageSize, MaxPackageSize, TruckCapacity) ->
     if
-        PackageSize > TruckCapacity ->
-            exit({error, "Package size cannot be greater than truck capacity"});
+        MinPackageSize > MaxPackageSize ->
+            exit({error, "Min package size cannot be greater than max package size"});
+        MaxPackageSize > TruckCapacity ->
+            exit({error, "Max package size cannot be greater than truck capacity"});
         true ->
             TruckPids = [truck:start(Index, TruckCapacity) || Index <- lists:seq(1, NumTrucks)],
             ConveyorPids = [conveyor_belt:start(TruckPids) || _ <- lists:seq(1, NumBelts)],
-            package_generator:start(ConveyorPids, NumPackages, PackageSize),
+            package_generator:start(ConveyorPids, NumPackages, MinPackageSize, MaxPackageSize),
             io:format(
-                "Factory started with ~p packages of size ~p, ~p conveyor belts, ~p trucks, and truck capacity of ~p~n",
-                [NumPackages, PackageSize, NumBelts, NumTrucks, TruckCapacity]
+                "Factory started with ~p packages, package size range [~p,~p], ~p conveyor belts, ~p trucks, and truck capacity of ~p~n",
+                [NumPackages, MinPackageSize, MaxPackageSize, NumBelts, NumTrucks, TruckCapacity]
             )
     end.
--- task2/package_generator.erl
+++ task2/package_generator.erl
@@ -1,17 +1,18 @@
 -module(package_generator).
--export([start/3, loop/4]).
+-export([start/4, loop/5]).
 
-start(Conveyors, NumPackages, PackageSize) ->
-    spawn(?MODULE, loop, [Conveyors, NumPackages, 1, PackageSize]).
+start(Conveyors, NumPackages, MinPackageSize, MaxPackageSize) ->
+    spawn(?MODULE, loop, [Conveyors, NumPackages, 1, MinPackageSize, MaxPackageSize]).
 
-loop(_Conveyors, 0, _PackageId, _PackageSize) ->
+loop(_Conveyors, 0, _PackageId, _MinPackageSize, _MaxPackageSize) ->
     io:format("Package Generator: Finished creating packages...~n"),
     ok;
-loop(Conveyors, RemainingPackages, PackageId, PackageSize) ->
+loop(Conveyors, RemainingPackages, PackageId, MinPackageSize, MaxPackageSize) ->
+    PackageSize = rand:uniform(MaxPackageSize - MinPackageSize + 1) + MinPackageSize - 1,
     Package = {package, PackageId, PackageSize},
     RandomConveyor = lists:nth(rand:uniform(length(Conveyors)), Conveyors),
     RandomConveyor ! {new_package, Package},
-    io:format("Package Generator: Created package ~p of size ~p and sent to conveyor belt~p~n", [
+    io:format("Package Generator: Created package ~p of size ~p and sent to conveyor belt ~p~n", [
         PackageId, PackageSize, RandomConveyor
     ]),
-    loop(Conveyors, RemainingPackages - 1, PackageId + 1, PackageSize).
+    loop(Conveyors, RemainingPackages - 1, PackageId + 1, MinPackageSize, MaxPackageSize).
