707a322554a1177cf8d4e2b7f1e14c5ec9ce70dd
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2024-11-25 16:53:52 +0000

    Task 3 done - added random dealy between [1,MAX_DELAY_MS] and conveyor belts stop if no truck is available

--- task3/truck.erl
+++ task3/truck.erl
@@ -1,5 +1,6 @@
 -module(truck).
 -export([start/2, loop/3]).
+-define(MAX_DELAY_MS, 1000).
 
 start(TruckId, Capacity) ->
     spawn(?MODULE, loop, [TruckId, Capacity, Capacity]).
@@ -16,10 +17,14 @@ loop(TruckId, RemainingCapacity, OriginalCapacity) ->
                     loop(TruckId, NewCapacity, OriginalCapacity);
                 false ->
                     ConveyorPid ! {truck_full, self()},
-                    io:format("Truck ~p is full. Resetting...~n", [self()]),
-                    io:format("Truck ~p replaced and is now available.~n", [self()]),
+                    io:format("Truck ~p is full. Replacing with a new truck...~n", [self()]),
+                    random_delay(),
+                    io:format("Truck ~p is available again.~n", [self()]),
                     ConveyorPid ! {truck_available, self()},
                     self() ! {load_package, ConveyorPid, {package, PackageId, PackageSize}},
                     loop(TruckId, OriginalCapacity, OriginalCapacity)
             end
     end.
+
+random_delay() ->
+    rand:uniform(?MAX_DELAY_MS).
