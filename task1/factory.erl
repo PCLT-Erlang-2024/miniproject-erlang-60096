805759d81f9f13aed75d3f787fcc42a96f9619b2
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2024-11-25 07:35:02 +0000

    Task 1 done - now checking if package size is greater than truck capacity; if so throw error

--- task1/factory.erl
+++ task1/factory.erl
@@ -2,11 +2,15 @@
 -export([start/5]).
 
 start(NumPackages, NumBelts, NumTrucks, PackageSize, TruckCapacity) ->
-    TruckPids = [truck:start(Index, TruckCapacity) || Index <- lists:seq(1, NumTrucks)],
-    ConveyorPids = [conveyor_belt:start(TruckPids) || _ <- lists:seq(1, NumBelts)],
-    PackageGenerator = package_generator:start(ConveyorPids, NumPackages, PackageSize),
-    io:format(
-        "Factory started with ~p packages of size ~p, ~p conveyor belt(s), ~p truck(s), and truck capacity of ~p~n",
-        [NumPackages, PackageSize, NumBelts, NumTrucks, TruckCapacity]
-    ),
-    {ok, [PackageGenerator, ConveyorPids, TruckPids]}.
+    if
+        PackageSize > TruckCapacity ->
+            exit({error, "Package size cannot be greater than truck capacity"});
+        true ->
+            TruckPids = [truck:start(Index, TruckCapacity) || Index <- lists:seq(1, NumTrucks)],
+            ConveyorPids = [conveyor_belt:start(TruckPids) || _ <- lists:seq(1, NumBelts)],
+            package_generator:start(ConveyorPids, NumPackages, PackageSize),
+            io:format(
+                "Factory started with ~p packages of size ~p, ~p conveyor belts, ~p trucks, and truck capacity of ~p~n",
+                [NumPackages, PackageSize, NumBelts, NumTrucks, TruckCapacity]
+            )
+    end.
