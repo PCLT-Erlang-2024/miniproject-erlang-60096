15cc3339296c7dea6fba6e28233d136806d4b46d
jgfPereira <jgf.pereira@campus.fct.unl.pt>
2024-11-25 03:49:53 +0000

    Added truck logic

---
+++ task1/truck.erl
@@ -0,0 +1,23 @@
+-module(truck).
+-export([start/1, loop/2]).
+
+start(TruckId) ->
+    Capacity = 50,
+    spawn(?MODULE, loop, [TruckId, Capacity]).
+
+loop(TruckId, RemainingCapacity) ->
+    receive
+        {load_package, ConveyorPid, {package, PackageId, PackageSize}} ->
+            case PackageSize =< RemainingCapacity of
+                true ->
+                    io:format("Truck ~p loaded package ~p (size: ~p). Remaining capacity: ~p~n", [
+                        TruckId, PackageId, PackageSize, RemainingCapacity - PackageSize
+                    ]),
+                    loop(TruckId, RemainingCapacity - PackageSize);
+                false ->
+                    io:format("Truck ~p is full. Resetting...~n", [TruckId]),
+                    io:format("Truck ~p replaced.~n", [TruckId]),
+                    ConveyorPid ! {truck_replaced},
+                    loop(TruckId, 50)
+            end
+    end.
